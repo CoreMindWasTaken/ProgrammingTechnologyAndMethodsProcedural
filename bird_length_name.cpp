fb90fa236477812b45fae6e0744858d6a1947c10
CoreMind <roma.shimanovich.98@mail.ru>
2021-04-12 22:54:10 +0700

    New function (length name)

---
+++ bird_length_name.cpp
@@ -0,0 +1,8 @@
+#include "bird_atd.h"
+using namespace std;
+namespace Animals {
+int bird_length_name(Bird * B)
+{
+    return B->Name.size();
+}
+}
--- node_out.cpp
+++ node_out.cpp
@@ -2,12 +2,17 @@
 using namespace std;
 namespace Animals {
 bool animal_out(Animal * A, ofstream & ofst);
+int animal_length_name(Animal * A);
 bool node_out(Node * N, ofstream & ofst)
 {
     if (!animal_out(N->A, ofst))
     {
         return false;
     }
-    return true;
+    else
+    {
+        ofst << "Length animal name: " << animal_length_name(N->A) << endl;
+        return true;
+    }
 }
 }
